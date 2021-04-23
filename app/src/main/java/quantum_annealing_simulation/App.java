c19e950994e38a6146d172f41ab2689ea6d4233f
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-04-23 23:43:28 +0900

    add lib la4j

--- app/src/main/java/quantum_annealing_simulation/App.java
+++ app/src/main/java/quantum_annealing_simulation/App.java
@@ -3,7 +3,29 @@
  */
 package quantum_annealing_simulation;
 
+import org.la4j.Matrix;
+
 public class App {
+    final int Tau = 1;
+
+    public double scheduleE(double time) {
+        return time / this.Tau;
+    }
+
+    public double scheduleG(double time) {
+        return (Tau - time) / this.Tau;
+    }
+
+    public double create_tfim(double time, Matrix hamiltonian) {
+        double v = scheduleE(time);
+
+        if (hamiltonian == null) {
+
+        }
+
+        return 0;
+    }
+
     public static void main(String[] args) {
         final int N = 5;
         double E[] = new double[(int) Math.pow(2, N)];
